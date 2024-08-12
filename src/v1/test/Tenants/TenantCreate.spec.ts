1d1566846f2a7f8a258c832730a973a248f7aef6
md-Novfal <nofanovfal@gmail.com>
2024-08-12 17:11:05 +0530

    expected test case for database connection and create tenant had completed

--- src/v1/test/Tenants/TenantCreate.spec.ts
+++ src/v1/test/Tenants/TenantCreate.spec.ts
@@ -15,7 +15,7 @@ describe('TENANT CREATE API', () => {
     chai.spy.restore();
   });
 
-  it('IT SHOULD INSERT A NEW TENANT ALL THE REQUEST IS PROVIDED', (done) => {
+  it('Should insert tenent in to the database', (done) => {
     chai.spy.on(Tenant, 'findOne', () => {
       return Promise.resolve(null);
     });
@@ -37,7 +37,6 @@ describe('TENANT CREATE API', () => {
       return Promise.resolve(transactionMock);
     });
 
-    // Make the request and validate the response
     chai
       .request(app)
       .post('/api/v1/tenant/create')
@@ -53,6 +52,94 @@ describe('TENANT CREATE API', () => {
         res.should.have.status(200);
         res.body.should.be.a('object');
         res.body.params.status.should.be.eq('successful');
+        done();
+      });
+  });
+
+  it('Should not insert record in the database', (done) => {
+    chai.spy.on(AppDataSource, 'transaction', () => {
+      return Promise.reject(new Error('error occurred while connecting to the database'));
+    });
+
+    chai
+      .request(app)
+      .post('/api/v1/tenant/create')
+      .send({
+        tenant_name: 'test1',
+        tenant_type: 'Government',
+        is_active: true,
+        status: 'draft',
+        created_by: 'admin',
+      })
+      .end((err, res) => {
+        res.should.have.status(500);
+        res.body.should.be.a('object');
+        res.body.params.status.should.be.eq('failed');
+        done();
+      });
+  });
+
+  it('Should not insert record if already exists in the database', (done) => {
+    chai.spy.on(Tenant, 'findOne', () => {
+      return Promise.resolve({ tenant_name: 'mumbai' });
+    });
+
+    chai
+      .request(app)
+      .post('/api/v1/tenant/create')
+      .send({
+        tenant_name: 'mumbai',
+        tenant_type: 'Government',
+        is_active: true,
+        status: 'draft',
+        created_by: 'admin',
+      })
+      .end((err, res) => {
+        res.should.have.status(409);
+        res.body.should.be.a('object');
+        res.body.params.status.should.be.eq('failed');
+        res.body.responseCode.should.be.eq('CONFLICT');
+        res.body.err.err.should.be.eq('CONFLICT');
+        done();
+      });
+  });
+
+  it('Should not insert record when request object contains missing fields', (done) => {
+    chai
+      .request(app)
+      .post('/api/v1/tenant/create')
+      .send({
+        tenant_type: 'Government',
+        is_active: true,
+        status: 'draft',
+      })
+      .end((err, res) => {
+        res.should.have.status(400);
+        res.body.should.be.a('object');
+        res.body.params.status.should.be.eq('failed');
+        res.body.responseCode.should.be.eq('CLIENT_ERROR');
+        res.body.err.err.should.be.eq('TENANT_INVALID_INPUT');
+        done();
+      });
+  });
+
+  it('Should not insert record when given invalid schema', (done) => {
+    chai
+      .request(app)
+      .post('/api/v1/tenant/create')
+      .send({
+        tenant_name: 123,
+        tenant_type: 'Government',
+        is_active: true,
+        status: 'draft',
+        created_by: 'admin',
+      })
+      .end((err, res) => {
+        res.should.have.status(400);
+        res.body.should.be.a('object');
+        res.body.params.status.should.be.eq('failed');
+        res.body.responseCode.should.be.eq('CLIENT_ERROR');
+        res.body.err.err.should.be.eq('TENANT_INVALID_INPUT');
         done();
       });
   });
